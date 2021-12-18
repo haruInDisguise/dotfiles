3f5635570f02f979623c6458e204bebc1e0ec30a
haruInDisguise <justhoro@protonmail.com>
2021-12-18 15:05:00 +0100

    ZSH: Removed redundant function

--- config/zsh/config/scripts.zsh
+++ config/zsh/config/scripts.zsh
@@ -1,7 +1,4 @@
-# Wrapper for 'man'. Properly align display in narrow windows
-# TODO: This might become an alias...
-man() MANWIDTH="$COLUMNS" command -p man "$@"
-
-# TODO: Write a proper implementation
-kbmap() command -p setxkbmap "$1" -option caps:escape
+# Quickly switch kbmap, while preserving xkb config
+# TODO: Write a proper implementation...
+kbmap() command -p setxkbmap "${1:?error: missing keymap argument}" -option caps:escape
 
