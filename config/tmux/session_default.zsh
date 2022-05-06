98184294f01d7db6e79e50c7e12e67ef4ecaeaa4
haruInDisguise <justhoro@protonmail.com>
2022-05-06 16:23:54 +0200

    tmux: fixed config

--- config/tmux/session_default.zsh
+++ config/tmux/session_default.zsh
@@ -1,15 +1,14 @@
-#!/usr/bin/zsh
+# Launch/Attach to tmux and setup a basic session
+# To be sourced by a (somewhat) POSIX compliant shell.
 
-# Launch the default tmux session
 SESSION_NAME='dev'
 
-if [[ $(tmux list-sessions) > 0  ]]; then
-    exec tmux attach
-fi
+tmux list-sessions && exec tmux attach
 
-tmux start-server
-tmux new -s "${SESSION_NAME}" -d
-tiux new-window -t "${SESSION_NAME}"
+tmux new-session -s "${SESSION_NAME}" -d
+tmux new-window -t "${SESSION_NAME}"
+tmux new-window -t "${SESSION_NAME}"
+tmux select-window -t "${SESSION_NAME}:1"
 tmux select-window -t "${SESSION_NAME}:0"
 
 exec tmux attach -t "${SESSION_NAME}"
