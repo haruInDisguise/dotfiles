95ccf0f1e51ca79a2b5cbbb4768cd29ab85575ad
haruInDisguise <justhoro@protonmail.com>
2024-05-27 18:13:36 +0200

    [lib/log]: Actually implement basic logging...

--- lib/log.sh
+++ lib/log.sh
@@ -4,32 +4,53 @@
 if [ -z "$_DOTFILES_LOG_SH_" ]; then
     _DOTFILES_LOG_SH_=1
 
-    LOG_LEVEL_DEBUG="$(tput setaf 4)debug$(tput sgr0)"
-    LOG_LEVEL_INFO="$(tput setaf 2)info $(tput sgr0)"
-    LOG_LEVEL_WARN="$(tput setaf 3)warn $(tput sgr0)"
-    LOG_LEVEL_ERROR="$(tput setaf 1)error$(tput sgr0)"
+    _LOG_LEVEL=0
+    LOG_LEVEL="${LOG_LEVEL:-all}"
+    case "$LOG_LEVEL" in
+        'all') _LOG_LEVEL=0;;
+        'debug') _LOG_LEVEL=1;;
+        'info') _LOG_LEVEL=2;;
+        'warn') _LOG_LEVEL=3;;
+        'error') _LOG_LEVEL=4;;
+        'none') _LOG_LEVEL=5;;
+    esac
 
     _log_fmt() {
         _log_fmt_level="$1"; shift
         _log_fmt_msg="$@"
-        _log_fmt_date="$(tput sgr0) $(date -u '+%Y-%m-%dT%H:%M:%SZ')$(tput sgr0)"
+        _log_fmt_date="$(tput sgr0)$(date -u '+%Y-%m-%dT%H:%M:%SZ')$(tput sgr0)"
+        _log_fmt_prefix="$(tput setaf 0)["
+        _log_fmt_postfix="$(tput sgr0)${LOG_NAME:-dotfiles} $(tput setaf 0)]$(tput sgr0)"
 
-        echo -e "$(tput setaf 0)[$_log_fmt_date\t $_log_fmt_level$(tput setaf 0)]$(tput sgr0) $_log_fmt_msg"
+        echo "$_log_fmt_prefix$_log_fmt_date $_log_fmt_level $_log_fmt_postfix $_log_fmt_msg"
     }
 
     log_debug() {
-        _log_fmt "$LOG_LEVEL_DEBUG" $@
+        if [ "$_LOG_LEVEL" -le 1 ]; then
+            _log_fmt "$(tput setaf 4)DEBUG$(tput sgr0)" $@
+        fi
     }
 
     log_info() {
-        _log_fmt "$LOG_LEVEL_INFO" $@
+        if [ "$_LOG_LEVEL" -le 2 ]; then
+            _log_fmt "$(tput setaf 2)INFO $(tput sgr0)" $@
+        fi
     }
 
     log_warn() {
-        _log_fmt "$LOG_LEVEL_WARN" $@
+        if [ "$_LOG_LEVEL" -le 3 ]; then
+            _log_fmt "$(tput setaf 3)WARN $(tput sgr0)" $@
+        fi
     }
 
     log_error() {
-        _log_fmt "$LOG_LEVEL_ERROR" $@ >&2
+        if [ "$_LOG_LEVEL" -le 4 ]; then
+            _log_fmt "$(tput setaf 1)ERROR$(tput sgr0)" $@ >&2
+        fi
+    }
+
+    error_and_exit() {
+        log_error $@
+        exit 1
     }
 fi
