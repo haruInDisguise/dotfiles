35f18c316139b2f5b6d3f4fc7521f8475a042ed7
haruInDisguise <justhoro@protonmail.com>
2024-05-27 18:32:36 +0200

    [config]: Added additional install targets +open
    - Adjusted udev rules for my keyboard
    - (finally..) Added simple logging library

--- lib/notify.sh
+++ lib/notify.sh
@@ -1,6 +1,16 @@
 # Some useful functions for working with notifications
 # To be sourced
 
-notify_debug() {
+if [ -z "$_DOTFILES_NOTIFY_SH_" ]; then
+    _DOTFILES_NOTIFY_SH_=1
 
-}
+    NOTIFY_CMD="notify-send"
+
+    notify_evoke() {
+        #notify_send_value="${notify_send_value:-$(notify-send -p $@)}"
+        eval "$NOTIFY_CMD" $@
+    }
+
+    notify_evoke hello world
+    notify_evoke hello world
+fi
